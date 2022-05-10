0066fa1169383bfcd1b92a144e778fe2c6364e56
Eric Moraes <ersmoraes@outlook.com>
2022-05-10 14:42:25 -0300

    Backend da aplicação

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors';
+import { routes } from './routes';
+
+const app = express();
+
+// GET, POST, PUT, PATCH, DELET
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar um informação única de uma entidade
+// DELET = Deletar informações
+
+app.use(express.json());
+app.use(routes);
+app.use(cors())
+
+app.listen(3333, () => {
+  console.log('HTTP server running!');
+})
+
